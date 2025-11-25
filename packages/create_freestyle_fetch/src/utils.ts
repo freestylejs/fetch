2db349413b1cda01909bb5667db131039e419316
Danpa_cho <danpa725@cau.ac.kr>
2025-11-25 18:37:56 +0900

    fix: dot name conversion error for pascal case

--- packages/create_freestyle_fetch/src/utils.ts
+++ packages/create_freestyle_fetch/src/utils.ts
@@ -13,7 +13,7 @@
 export function toPascalCase(str: string): string {
     if (!str) return str
 
-    if (!/[\s\-_]/.test(str)) {
+    if (!/[\s\-_.]/.test(str)) {
         const words = str
             .replace(/([a-z])([A-Z])/g, '$1 $2') // Split camelCase: "userId" -> "user Id"
             .replace(/([A-Z]+)([A-Z][a-z])/g, '$1 $2') // Split "XMLParser" -> "XML Parser"
@@ -32,12 +32,12 @@ export function toPascalCase(str: string): string {
     const words = str
         .replace(/([a-z])([A-Z])/g, '$1 $2') // Split camelCase: "userId" -> "user Id"
         .replace(/([A-Z]+)([A-Z][a-z])/g, '$1 $2') // Split "XMLParser" -> "XML Parser"
-        .split(/[\s\-_]+/) // Split on spaces, hyphens, underscores
+        .split(/[\s\-_.]+/) // Split on spaces, hyphens, underscores, and dots
         .filter((word) => word.length > 0)
 
     return words
         .map((word) => {
-            // Preserve leading special characters (but not underscore which is a separator)
+            // Preserve leading special characters (but not underscore/dot which are separators)
             const match = word.match(/^([^a-zA-Z0-9]*)(.*)$/)
             if (!match) return word
 
