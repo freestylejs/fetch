71dbd7891250a40018a65b75b17c29cbd7eadd85
Danpa_cho <danpa725@cau.ac.kr>
2025-11-25 12:40:07 +0900

    refactor: use middleware builder, instead class

--- packages/fetch/src/index.ts
+++ packages/fetch/src/index.ts
@@ -1,18 +1,18 @@
 import { builder, type FetchBuilder, type FetchUnitShape } from './core/fetcher'
 import { type GetRouterConfig, router } from './core/router'
-import { Middleware } from './utils/middleware'
+import { type Middleware, middleware } from './utils/middleware'
 
 /**
- * @description Metal Fetch Root
+ * Fetch core namespace
  */
 export const f: {
     builder: typeof builder
     router: typeof router
-    Middleware: typeof Middleware
+    middleware: () => Middleware
 } = {
     builder,
     router,
-    Middleware,
+    middleware,
 }
 
 export type { GetRouterConfig, FetchUnitShape, FetchBuilder }
